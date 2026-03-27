1a61ac5dd1ceeedc4985a0ee01ba1eda064f82cd
Aryan <aryanghugare75@gmail.com>
2026-03-27 13:41:11 +0530

    feat: Add Resume and User models

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './configs/db.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Database connection
+await connectDB();
 
 
 app.use(cors());
