b468c318723df32ff1ec7ce8f27d30a41fad61c9
Aryan <aryanghugare75@gmail.com>
2026-03-27 18:28:04 +0530

    Resume controller and multer middleware

---
+++ server/controllers/resumeController.js
@@ -0,0 +1,85 @@
+import { asyncHandler } from "../utils/AsyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import Resume from "../models/Resume.js"
+
+
+
+export const createResume = asyncHandler(async(req,res)=>{
+const userId = req.userId;
+        const {title} = req.body;
+
+        // create new resume
+        const newResume = await Resume.create({userId, title})
+            if(!newResume) throw new ApiError(500, "Failed to create resume")
+        // return success message
+        return res.status(201).json({message: 'Resume created successfully', resume: newResume})
+
+
+})
+
+
+export const deleteResume = asyncHandler(async(req,res)=>{
+
+const userId = req.userId;
+        const {resumeId} = req.params;
+
+       await Resume.findOneAndDelete({userId, _id: resumeId})
+
+        // return success message
+        return res.status(200).json({message: 'Resume deleted successfully'})
+
+
+})
+
+
+export const getResumeById = asyncHandler(async(req,res)=>{
+const userId = req.userId;
+        const {resumeId} = req.params;
+
+       const resume = await Resume.findOne({userId, _id: resumeId})
+
+       if(!resume){
+      throw new ApiError(404, "Resume not found");
+       }
+
+        resume.__v = undefined;
+        resume.createdAt = undefined;
+        resume.updatedAt = undefined;
+
+        return res.status(200).json({resume})
+
+
+
+})
+
+
+
+export const getPublicResumeById = asyncHandler(async(req,res)=>{
+
+const { resumeId } = req.params;
+        const resume = await Resume.findOne({public: true, _id: resumeId})
+
+        if(!resume){
+       throw new ApiError(404, "Resume not found");
+       }
+
+       return res.status(200).json({resume})
+
+
+})
+
+
+// controller for updating a resume 
+// PUT: /api/resumes/update 
+
+
+export const updateResume = asyncHandler(async(req,res)=>{
+ const userId = req.userId;
+        const {resumeId, resumeData, removeBackground} = req.body
+        const image = req.file;
+
+
+
+
+
+})
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './configs/db.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -19,6 +20,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/api/users', userRouter)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
