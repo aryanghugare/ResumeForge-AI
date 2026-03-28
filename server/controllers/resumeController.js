d568014f42d1703d47d59e60ed5b50c7513728b1
Aryan <aryanghugare75@gmail.com>
2026-03-28 07:58:46 +0530

    feat: Integrate ImageKit for resume image processing and update resume functionality

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from "../utils/AsyncHandler.js"
 import { ApiError } from "../utils/ApiError.js"
 import Resume from "../models/Resume.js"
+import imagekit from "../configs/imageKit.js"
+import fs from 'fs';
 
 
 
@@ -77,7 +79,34 @@ export const updateResume = asyncHandler(async(req,res)=>{
  const userId = req.userId;
         const {resumeId, resumeData, removeBackground} = req.body
         const image = req.file;
+        
+        let resumeDataCopy; 
+        if(typeof resumeData === 'string'){
+            resumeDataCopy = await JSON.parse(resumeData)
+        }else{
+            resumeDataCopy = structuredClone(resumeData)
+        }
 
+        if(image){
+            
+
+            const imageBufferData = fs.createReadStream(image.path)
+
+            const response = await imagekit.files.upload({
+                            file: imageBufferData,
+                            fileName: 'resume.png',
+                            folder: 'user-resumes',
+                             transformation: {
+                                pre: 'w-300,h-300,fo-face,z-0.75' + (removeBackground ? ',e-bgremove' : '')
+                             }
+                            });
+
+            resumeDataCopy.personal_info.image = response.url
+        }
+
+       const resume = await Resume.findByIdAndUpdate({userId, _id: resumeId}, resumeDataCopy, {new: true})
+
+       return res.status(200).json({message: 'Saved successfully', resume})
 
 
 
